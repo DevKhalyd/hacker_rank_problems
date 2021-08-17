4339628223003407f10c1cd57a3592b48a43568b
Rolando Garcia <jrdosmil0@gmail.com>
2021-08-16 19:42:02 -0500

    Interfaces Problem fixex

---
+++ interfaces/main.py
@@ -0,0 +1,19 @@
+# Interface to implement
+class AdvancedArithmetic(object):
+    def divisorSum(_):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n) -> int:
+        total = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                total += i
+        return total
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
--- test/main.py
+++
@@ -1,3 +0,0 @@
-gello = "asdsadasd"
-
-print(gello)
