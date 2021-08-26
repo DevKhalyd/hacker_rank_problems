4ee8c019169f26ce7f34e5290d72c2588143115b
Rolando Garcia <jrdosmil0@gmail.com>
2021-08-25 22:52:29 -0500

    BTS added

--- binary_three/main.py
+++ binary_three/main.py
@@ -1,11 +1,18 @@
+
 class Node:
     def __init__(self, data):
         self.right = self.left = None
         self.data = data
 
     def __str__(self) -> str:
-        # TODO: Print the left and right values
-        return str(self.data)
+        data = self.data
+        right = self.right
+        left = self.left
+        return str(left) + str(data) + str(right)
+
+    def printDef(self) -> None:
+        definition = self.__str__()
+        print(definition)
 
 
 class Solution:
@@ -21,27 +28,29 @@ class Solution:
                 root.right = cur
         return root
 
-    def getHeight(self, root: Node):
-        # Write your code here
-        print("Height")
-        print(root)
-        pass
-
-    # https://stackoverflow.com/questions/2597637/finding-height-in-binary-search-tree
-
+    # https://stackoverflow.com/a/2597754/10942018
+    # Basically here I learned the concept of the binary three search
+    # Check other section in this repo to see my own solutions for this one
+    def getHeight(self, root: Node) -> int:
+        if root is None:
+            return -1
+        lefth = self.getHeight(root.left)
+        righth = self.getHeight(root.right)
+        if lefth > righth:
+            return lefth + 1
+        else:
+            return righth + 1
 
 def main() -> None:
-    print('Start program')
     T = int(input())
-    print('Number to insert: ', T)
     myTree = Solution()
     root = None
     for i in range(T):
         data = int(input())
         root = myTree.insert(root, data)
+    # TODO: Uncomment this
     height = myTree.getHeight(root)
     print(height)
-
 
 if __name__ == "__main__":
     main()
