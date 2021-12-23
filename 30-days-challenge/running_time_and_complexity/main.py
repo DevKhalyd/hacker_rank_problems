3add02ac25efaf1d10f304b83fadf134d1f491b3
Rolando Garcia <jrdosmil0@gmail.com>
2021-12-23 11:38:49 -0600

    Time Complexity Added

---
+++ 30-days-challenge/running_time_and_complexity/main.py
@@ -0,0 +1,12 @@
+"""
+Task:
+
+A prime is a natural number greater than 1 that has no positive 
+divisors other than 1 and itself. Given a number,n , determine 
+and print whether it is Prime or Not prime.
+
+Ref:https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem?isFullScreen=true
+
+"""
+
+#  Enter your code here. Read input from STDIN. Print output to STDOUT
