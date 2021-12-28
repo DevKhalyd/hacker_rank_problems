296d99b2e0f11c844c07a428302789c3e7147d1d
Rolando Garcia <jrdosmil0@gmail.com>
2021-12-28 12:35:15 -0600

    Run and complexity solution added not enough for the problem

--- 30-days-challenge/running_time_and_complexity/main.py
+++ 30-days-challenge/running_time_and_complexity/main.py
@@ -10,3 +10,37 @@ Ref:https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
 """
 
 #  Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# NOTE: This code works for the first 10 numbers but what if I have a number bigger???
+
+# TODO: Complete with another algorithm
+
+prime = 'Prime'
+not_prime = 'Not prime'
+
+T = int(input())
+
+list = []
+
+for i in range(T):
+    n = int(input())
+    val = None
+    if n % n == 0 and n % 1 == 0:
+        for index in range(2,7):
+            if index == n:
+                continue
+
+            if n % index == 0:
+                val = not_prime
+            
+            if val:
+                break
+
+        if not val:
+            val = prime
+
+    list.append(val)
+
+
+for s in list:
+    print(s)
