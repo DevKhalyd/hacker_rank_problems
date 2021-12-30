09038fc45772bdd739f948ea5be3fed5b0f26d13
Rolando Garcia <jrdosmil0@gmail.com>
2021-12-30 08:36:59 -0600

    New problem added

---
+++ 30-days-challenge/nested_logic/main.py
@@ -0,0 +1,16 @@
+"""
+Ref: https://www.hackerrank.com/challenges/30-nested-logic/problem?isFullScreen=false
+
+Your local library needs your help! Given the expected and actual 
+return dates for a library book, create a program that calculates 
+the fine (if any).
+
+INPUT SAMPLE:
+
+9 6 2015 (Data returned)
+6 6 2015 (Data due)
+
+Two lines separated by a space containing 3 integers.
+
+"""
+
