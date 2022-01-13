75e172e7269087c0adacf64e07f86a89cf1f217c
Rolando Garcia <jrdosmil0@gmail.com>
2022-01-13 10:17:52 -0600

    Solution not found

--- 30-days-challenge/bitwese_AND/main.py
+++ 30-days-challenge/bitwese_AND/main.py
@@ -1,7 +1,3 @@
-#!/bin/python3
-
-import os
-
 #
 # Complete the 'bitwiseAnd' function below.
 #
@@ -11,28 +7,54 @@ import os
 #  2. INTEGER K
 #
 
+"""
+Refs:
+
+
+https://docs.microsoft.com/en-us/cpp/cpp/bitwise-and-operator-amp?view=msvc-170
+
+https://dartpad.dev/?id Check the result of 1&2 expression
+
+"""
+
 # TODO Undestand the bitwise AND operator
 
+
 def bitwiseAnd(N, K):
-    # Write your code here
-    pass
+    """N: limit of the set of numbers
+       K: The limit to return in this function
+    """
+
+    list = range(1, N)
+    # Max Possible
+    mP = 0
+
+    for _, val in enumerate(list):
+
+        # For in each value of the list
+
+        for x in range(val, N):
+            A = x + 1
+            B = A + 1
+
+            if B > N:
+                break
+
+            result = A & B
+
+            if result < K and result > mP:
+                mP = result
+    return mP
+
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    t = int(input().strip())
+    # Test not passed with 5 2...
+    count = 5
 
-    for t_itr in range(t):
-        first_multiple_input = input().rstrip().split()
+    lim = 2
 
-        count = int(first_multiple_input[0])
+    res = bitwiseAnd(count, lim)
 
-        lim = int(first_multiple_input[1])
-
-        res = bitwiseAnd(count, lim)
-
-        print(res)
-
-        #fptr.write(str(res) + '\n')
-
-    #fptr.close()
+    print(res)
