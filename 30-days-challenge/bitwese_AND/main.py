57305fa5e8334f2d5ddf1a31afaaa2725efd45df
Rolando Garcia <jrdosmil0@gmail.com>
2022-01-18 10:02:09 -0600

    A optimization added

--- 30-days-challenge/bitwese_AND/main.py
+++ 30-days-challenge/bitwese_AND/main.py
@@ -7,18 +7,13 @@
 #  2. INTEGER K
 #
 
+import math
+
 """
 Refs:
-
-
 https://docs.microsoft.com/en-us/cpp/cpp/bitwise-and-operator-amp?view=msvc-170
-
 https://dartpad.dev/?id Check the result of 1&2 expression
-
 """
-
-# TODO: Test against the hacker rank test
-
 
 def bitwiseAnd(N, K):
     """N: limit of the set of numbers
@@ -26,18 +21,27 @@ def bitwiseAnd(N, K):
     """
 
     list = range(1, N)
-    # Max Possible
+    # Max Possible value
     mP = 0
 
     for val in list:
 
         A = val
 
-        # As a possible solution is to get the root of N...
+        iterations = 0
+        # Avoid iterate a lot of times more
+        if mP != 0 and mP == K-1:
+            break
+
         for x in range(val, N):
+            iterations += 1
             B = x + 1
 
+            # Not in the set of numbers
             if B > N:
+                break
+
+            if mP != 0 and mP == K-1:
                 break
 
             result = A & B
@@ -52,9 +56,9 @@ if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     # Test not passed with 5 2...
-    count = 8
+    count = 2
 
-    lim = 5
+    lim = 2
 
     res = bitwiseAnd(count, lim)
 
