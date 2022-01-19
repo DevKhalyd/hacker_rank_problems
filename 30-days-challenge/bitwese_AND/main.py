11166a78db5263baeec250b28a7149b71abe0711
Rolando Garcia <jrdosmil0@gmail.com>
2022-01-19 10:33:24 -0600

    problem almost solved

--- 30-days-challenge/bitwese_AND/main.py
+++ 30-days-challenge/bitwese_AND/main.py
@@ -6,16 +6,18 @@
 #  1. INTEGER N
 #  2. INTEGER K
 #
-
-import math
-
 """
 Refs:
 https://docs.microsoft.com/en-us/cpp/cpp/bitwise-and-operator-amp?view=msvc-170
 https://dartpad.dev/?id Check the result of 1&2 expression
 """
+# Rewrite this function with the new data found and test against the problems
+def bitwiseAnd(n, k):
 
-def bitwiseAnd(N, K):
+    pass
+
+
+def bitwiseAnd2(N, K):
     """N: limit of the set of numbers
        K: The limit to return in this function
     """
@@ -26,14 +28,15 @@ def bitwiseAnd(N, K):
 
     for val in list:
 
-        A = val
+        # TODO: Just use K - 1
+        A = K - 1
 
         iterations = 0
         # Avoid iterate a lot of times more
         if mP != 0 and mP == K-1:
             break
 
-        for x in range(val, N):
+        for x in range(A, N):
             iterations += 1
             B = x + 1
 
@@ -41,13 +44,15 @@ def bitwiseAnd(N, K):
             if B > N:
                 break
 
-            if mP != 0 and mP == K-1:
-                break
-
             result = A & B
+
+            print(f"{A} & {B} = {result}. Find Min: {K - 1}")
 
             if result < K and result > mP:
                 mP = result
+
+            if mP != 0 and mP == K-1:
+                break
 
     return mP
 
@@ -56,9 +61,13 @@ if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     # Test not passed with 5 2...
-    count = 2
 
-    lim = 2
+    # TODO:Test the other cases
+
+    count = 614
+
+    lim = 327
+    # Expected 103 getting 106. Why?
 
     res = bitwiseAnd(count, lim)
 
